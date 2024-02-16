577a4e881f6a36cecea6bc5782ee8ef4f822bc30
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-02-16 23:28:34 +0200

    feat: Include language localization data

---
+++ src/languages/languages.js
@@ -0,0 +1,54 @@
+export const langs = {
+    heading: {
+        en: 'Jokes Mystery Box',
+        bg: 'Тайната кутия с шеги'
+    },
+    loadingMessage: {
+        en: "Searching a joke from the box ...",
+        bg: "Търси се шега от кутията ..."
+    },
+    errorMessage: {
+        en: 'There are no jokes in the box!',
+        bg: 'Няма шеги в кутията'
+    },
+    langSwitch: {
+        en: "Language",
+        bg: "Език"
+    },
+    themeSwitch: {
+        en: "Theme",
+        bg: "Тема"
+    },
+    getAJoke: {
+        en: "Unveil a Joke",
+        bg: "Разкрий Шега"
+    },
+    jokeAnswer: {
+        en: 'A:',
+        bg: 'Отговор:'
+    },
+    jokeQuestion: {
+        en: 'Q:',
+        bg: 'Въпрос:'
+    },
+    langOptions: {
+        en: {
+            english: 'En',
+            bulgarian: 'Bg'
+        },
+        bg: {
+            english: 'Англ',
+            bulgarian: 'Бг'
+        }
+    },
+    themeOptions: {
+        en: {
+            light: 'Light',
+            dark: 'Dark'
+        },
+        bg: {
+            light: 'Светла',
+            dark: 'Тъмна'
+        }
+    }
+};
